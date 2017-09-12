8db32ffbd6159fdfaa157443887188e60a8dd008
natalianadgela <natalia.nadgela@gmail.com>
2017-09-12 19:55:10 -0300

    Versão 1

---
+++ src/jogovelha.py
@@ -0,0 +1,13 @@
+def inicializar():
+	tab= []
+		linha =[]
+		for j in range(3):
+			linhas.append(".")
+		return tab
+
+def mais ():
+	jogo = inicializador()
+	print (jogo)
+
+if_name_ == "_main_":
+	main()
---
+++ src/testes.py
@@ -0,0 +1,20 @@
+Import jogovelha
+import sys
+
+erroInicializar = false
+jogo = jogovelha.inicializar()
+
+if len(jogo) !3:
+	erroInicializar= True
+else:
+	for linha in jogo:
+		if len(linha) !=3:
+			erroInicializar = True
+		else:
+			for elemento in linha:
+				if elemneto != '.':
+					erroInicializar = True
+if erroInicializar:
+	sys.exit(1)
+else:
+	sys.exit(0)
